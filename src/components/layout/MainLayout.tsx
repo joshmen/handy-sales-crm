be31aed9ac2461e6c5c0ac36733deda9c5f159fb
jms <josue.mendoza@parking.net.co>
2025-08-15 02:11:19 -0700

    fix: resolver temp errors

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { Header } from './Header';
+import Header from './Header'; // ⬅️ default import
 import { Sidebar } from './Sidebar';
 import { MobileMenu } from './MobileMenu';
 
